b39355d3c6f11e0176885f3303e180fce7da4cf6
Saurav samal <sauravsamal889520@gmail.com>
2022-09-12 20:57:09 +0530

    assignment-13

--- assignment-12/1.c
+++ assignment-12/1.c
@@ -1,14 +1,16 @@
 #include<stdio.h>
 
-int sum(int n){
-    if(n==1)
-    return 1;
-    else
-    return n+sum(n-1);
+void print(int i,int n){
+    if(i<=n)
+   {
+     print(i,n-1);
+    printf("%d\n",n);
+    }
+
 }
 int main()
 {
     int n=10;
-   printf("%d",sum(n));
+  print(1,n);
     return 0;
 }
